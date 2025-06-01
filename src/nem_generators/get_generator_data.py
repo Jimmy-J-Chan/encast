e4490aa25f02aaa04011e154544b26e1e889a97a
Jimmy <jimmychan0507@gmail.com>
2025-06-01 17:43:13 +1000

    commit

--- src/nem_generators/get_generator_data.py
+++ src/nem_generators/get_generator_data.py
@@ -1,7 +1,9 @@
+import pandas as pd
+
 from conf.config import *
 import requests
 
-def get_raw_generator_data(file_path=None, fn_save=None, update=False):
+def get_raw_generator_data(file_path=None, fn_save=None, update=True):
     if update:
         # ExistingGeneration&NewDevs
         gens = pd.read_excel(file_path, sheet_name='ExistingGeneration&NewDevs', skiprows=1)
@@ -11,27 +13,68 @@ def get_raw_generator_data(file_path=None, fn_save=None, update=False):
         gens.to_pickle(fn_save)
     pass
 
-def get_generator_coords(fn_save=None, update=False):
-    gens = pd.read_pickle(fn_save)
+def generate_search_phrase(search_terms=None):
 
-    # region, site name,
-    tmp = requests.get('https://maps.googleapis.com/maps/api/geocode/json?key={}&address=adelaide%20desalination%20plant')
+
+
+    return search_phrase
+
+def get_generator_coords(fn_gendata=None, update=False, update_diff=False):
+    """
+    using google geocoding api
+    - neeed to get api key first and store into secrets folder
+    """
+
+    # load some data
+    import json
+    with open(config.project.folder + '/secrets/gmaps.json', "r") as f:
+        api_key = json.load(f)['key']
+    gendata = pd.read_pickle(fn_gendata)
+
+    # query string
+    query_str = 'https://maps.googleapis.com/maps/api/geocode/json?key={}&address={}'
+
+    # collect geocoding data
+    search_terms = ['Region','Site Name','Owner']
+    for ix, row in gendata.iterrows():
+        search_terms = row[search_terms]
+        search_phrase = generate_search_phrase(search_terms)
+        tmp = requests.get(query_str.format(api_key, search_phrase))
+        # tmp = requests.get(query_str.format(api_key, "adelaide desalination plant"))
     pass
 
 
 
 def get_generator_data():
-    # get raw generator data from AEMO
-    # https://aemo.com.au/energy-systems/electricity/national-electricity-market-nem/nem-forecasting-and-planning/forecasting-and-planning-data/generation-information
-    # Get latest excel file url - NEM Generation information publications
-    raw_file_path = r"U:\Research\Projects\sef\encast\NEM\generators\NEM-Generation-Information-April-2025.xlsx"
-    fn_save = r'U:\Research\Projects\sef\encast\NEM\generators\generator_data.pkl'
-    get_raw_generator_data(raw_file_path, fn_save, update=False)
+    # flags
+    update_raw_generator_data = False
+    update_gendata_addresses = False
+    update_generator_coords = True
 
-    # get coordinates for each generator
-    get_generator_coords(fn_save, update=True)
+    # file paths
+    fn_gendata = r'U:\Research\Projects\sef\encast\NEM\generators\generator_data.pkl' # qut
+    fn_gendata = r"U:\NEM\generators\generator_data.pkl" # pc
+    fn_gendata = r"C:\Users\Jimmy\OneDrive - Queensland University of Technology\Documents\encast\data\NEM\generator_data.pkl"
+    fn_gendata_xl = r"C:\Users\Jimmy\OneDrive - Queensland University of Technology\Documents\encast\data\NEM\generator_data.xlsx"
 
+    if update_raw_generator_data:
+        # get raw generator data from AEMO
+        # https://aemo.com.au/energy-systems/electricity/national-electricity-market-nem/nem-forecasting-and-planning/forecasting-and-planning-data/generation-information
+        # Get latest excel file url - NEM Generation information publications
+        raw_file_path = r"U:\Research\Projects\sef\encast\NEM\generators\NEM-Generation-Information-April-2025.xlsx"
+        raw_file_path = r"U:\NEM\generators\NEM-Generation-Information-April-2025.xlsx"
+        get_raw_generator_data(raw_file_path, fn_gendata)
 
+    if update_gendata_addresses:
+        # save to excel
+        df = pd.read_pickle(fn_gendata)
+        df['address_manual'] = None
+        df.to_excel(fn_gendata_xl, index=False)
+        # manually fill in addresses of generators - copied straight from google maps
+
+    if update_generator_coords:
+        # get coordinates for each generator
+        get_generator_coords(fn_gendata, update=True, update_diff=False)
 
 
     pass
--- src/nemdata_schd/download_nemdata_schd.py
+++ src/nemdata_schd/download_nemdata_schd.py
@@ -1,8 +1,11 @@
-from conf.config import *
-
+from src.nemdata_schd.download_nemdata_archive_schd import download_nem_archive_update
+from src.nemdata_schd.download_nemdata_current_schd import download_nem_current_update
 
 
 
 
 if __name__ == '__main__':
+    base_fn_save = ''
+    download_nem_archive_update(base_fn_save, update_metadata=True, save_metadata=True)
+    download_nem_current_update(base_fn_save, update_metadata=True, save_metadata=True)
     pass
